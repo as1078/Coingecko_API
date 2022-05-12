ecb22aea34dfa6a65e46a9129a4c0ae5ac062058
Aman Singh <aman.singh769@duke.edu>
2022-05-11 23:30:12 -0400

    Fragmented main page, spacing and text aligned

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Button from "./Button"
 import {Link} from "react-router-dom";
 
@@ -6,37 +6,37 @@ const List = props => (
     <div>
         <h1>
             Directory
-        </h1>    
+        </h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Logo</th>
+                    <th>Country</th>
+                    <th>URL</th>
+                    <th>Trust Rank</th>
+                </tr>
+            </thead>    
         {props.exchanges.map((obj)=>{
                 return(
-                    <div>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Logo</th>
-                                    <th>Country</th>
-                                    <th>URL</th>
-                                    <th>Trust Rank</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{obj.name}</td>
-                                    <td><img src={obj.image}/></td>
-                                    <td>{obj.country}</td>
-                                    <td><a href={obj.url}>{obj.url}</a></td>
-                                    <td>{obj.trust_score_rank}</td>
-                                    <td><button className="exchange_buttons">
-                                        <Link to={{pathname: `/crypexchange/${obj.id}`,
-                                        state:{crypexchange: obj.id}}
-                                    }>Exchange</Link></button></td>
-                                </tr>
-                            </tbody>
-                        </table> 
-                    </div>
-                    )
+                    <Fragment>   
+                        <tbody>
+                            <tr>
+                                <td>{obj.name}</td>
+                                <td><img src={obj.image}/></td>
+                                <td>{obj.country}</td>
+                                <td><a href={obj.url}>{obj.url}</a></td>
+                                <td>{obj.trust_score_rank}</td>
+                                <td><button className="exchange_buttons">
+                                    <Link to={{pathname: `/crypexchange/${obj.id}`,
+                                    state:{crypexchange: obj.id}}
+                                }>Exchange</Link></button></td>
+                            </tr>
+                        </tbody>
+                    </Fragment>
+                    )         
             })}
+            </table> 
     </div>
     
 );
